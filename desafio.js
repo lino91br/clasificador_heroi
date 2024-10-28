43ab06fa13b25862abf96bf76fec867e6e447d26
lino91br <tgpaulino1@gmail.com>
2024-10-28 20:34:46 -0300

    segundo commit

--- desafio.js
+++ desafio.js
@@ -1,1 +1,26 @@
-TextDecoderStream
+// Declaração das variáveis
+let nomeHeroi = "Lino91br";
+let experiencia = 7500;
+let nivel;
+
+// Estrutura de decisão para classificar o nível do herói com base na experiência (XP)
+if (experiencia < 1000) {
+    nivel = "Ferro";
+} else if (experiencia <= 2000) {
+    nivel = "Bronze";
+} else if (experiencia <= 5000) {
+    nivel = "Prata";
+} else if (experiencia <= 7000) {
+    nivel = "Ouro";
+} else if (experiencia <= 8000) {
+    nivel = "Platina";
+} else if (experiencia <= 9000) {
+    nivel = "Ascendente";
+} else if (experiencia <= 10000) {
+    nivel = "Imortal";
+} else {
+    nivel = "Radiante";
+}
+
+// Exibe a mensagem final com o nível do herói
+console.log(`O Herói de nome ${nomeHeroi} está no nível de ${nivel}`);
